7391c017e4cee9de55d75b8f2e5b196172917b2f
Qasim Ali <raorizwan@gmail.com>
2025-05-16 18:37:49 +0500

    changes

--- src/app/pricing-calculator/page.tsx
+++ src/app/pricing-calculator/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { Button } from '../../../components/ui/button'
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { useToast } from '@/components/ui/use-toast'
